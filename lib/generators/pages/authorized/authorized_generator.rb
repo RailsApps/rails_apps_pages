4e55e0fbf48036fd2e511b0bbe26d0a1f03310dc
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-01 16:47:07 -0700

    fixes for 'authorized pages' generator

--- lib/generators/pages/authorized/authorized_generator.rb
+++ lib/generators/pages/authorized/authorized_generator.rb
@@ -8,12 +8,14 @@ module Pages
       desc "Create pages to accompany a User model when authorization is available."
 
       def create_page
+        ### assumes we are using Pundit for authorization
         copy_file 'users/_user.html.erb', 'app/views/users/_user.html.erb'
         copy_file 'users/index.html.erb', 'app/views/users/index.html.erb'
         copy_file 'users/show.html.erb', 'app/views/users/show.html.erb'
         copy_file 'users_controller.rb', 'app/controllers/users_controller.rb'
+        copy_file 'user_policy.rb', 'app/policies/user_policy.rb'
         route = '  resources :users'
-        inject_into_file 'config/routes.rb', route + "\n", :before => "end"
+        inject_into_file 'config/routes.rb', route + "\n", :after => "devise_for :users\n"
         copy_file 'visitors/index.html.erb', 'app/views/visitors/index.html.erb'
         copy_file 'visitors_controller.rb', 'app/controllers/visitors_controller.rb'
         route = '  root :to => "visitors#index"'
