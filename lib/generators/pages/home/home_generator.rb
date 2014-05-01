b7f8303de21a1f08a33f899baf9660695089e490
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-01 10:19:38 -0700

    use a 'visitors' controller for the home page

--- lib/generators/pages/home/home_generator.rb
+++ lib/generators/pages/home/home_generator.rb
@@ -8,9 +8,9 @@ module Pages
       desc "Create a home page"
 
       def create_page
-        copy_file 'index.html.erb', 'app/views/home/index.html.erb'
-        copy_file 'home_controller.rb', 'app/controllers/home_controller.rb'
-        route = '  root :to => "home#index"'
+        copy_file 'index.html.erb', 'app/views/visitors/index.html.erb'
+        copy_file 'home_controller.rb', 'app/controllers/visitors_controller.rb'
+        route = '  root :to => "visitors#index"'
         inject_into_file 'config/routes.rb', route + "\n", :after => "routes.draw do\n"
       end
 
