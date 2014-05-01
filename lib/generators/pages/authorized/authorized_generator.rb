b781e2626de2fadd4728dfa33f0135265af4ac91
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-01 15:36:48 -0700

    generate user pages requiring authorization

---
+++ lib/generators/pages/authorized/authorized_generator.rb
@@ -0,0 +1,25 @@
+require 'rails/generators'
+
+module Pages
+  module Generators
+    class AuthorizedGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Create pages to accompany a User model when authorization is available."
+
+      def create_page
+        copy_file 'users/_user.html.erb', 'app/views/users/_user.html.erb'
+        copy_file 'users/index.html.erb', 'app/views/users/index.html.erb'
+        copy_file 'users/show.html.erb', 'app/views/users/show.html.erb'
+        copy_file 'users_controller.rb', 'app/controllers/users_controller.rb'
+        route = '  resources :users'
+        inject_into_file 'config/routes.rb', route + "\n", :before => "end"
+        copy_file 'visitors/index.html.erb', 'app/views/visitors/index.html.erb'
+        copy_file 'visitors_controller.rb', 'app/controllers/visitors_controller.rb'
+        route = '  root :to => "visitors#index"'
+        inject_into_file 'config/routes.rb', route + "\n", :after => "routes.draw do\n"
+      end
+
+    end
+  end
+end
