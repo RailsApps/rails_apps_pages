b3659d5ee5e5980a2761e2e543bc98c7cb2a959c
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-31 05:42:28 -0700

    user pages for OmniAuth

--- lib/generators/pages/users/users_generator.rb
+++ lib/generators/pages/users/users_generator.rb
@@ -5,32 +5,47 @@ module Pages
     class UsersGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Create pages to accompany a User model when authentication is available."
+      desc "Create pages to accompany a User model."
 
-      def create_page
-        ### assumes we are using Devise for authentication
+      def create_pages
         copy_file 'users/_user.html.erb', 'app/views/users/_user.html.erb'
         copy_file 'users/index.html.erb', 'app/views/users/index.html.erb'
         copy_file 'users/show.html.erb', 'app/views/users/show.html.erb'
-        copy_file 'users_controller.rb', 'app/controllers/users_controller.rb'
-        route = '  resources :users'
-        inject_into_file 'config/routes.rb', route + "\n", :after => "devise_for :users\n"
         generate 'pages:home -f'
         copy_file 'visitors/index.html.erb', 'app/views/visitors/index.html.erb'
       end
 
-      def add_name_field
+      def create_devise_pages
+        return unless File.exists?('config/initializers/devise.rb')
+        copy_file 'devise/users_controller.rb', 'app/controllers/users_controller.rb'
+        route = '  resources :users'
+        inject_into_file 'config/routes.rb', route + "\n", :after => "devise_for :users\n"
+      end
+
+      def create_omniauth_pages
+        return unless File.exists?('config/initializers/omniauth.rb')
+        copy_file 'users/edit.html.erb', 'app/views/users/edit.html.erb'
+        copy_file 'omniauth/users_controller.rb', 'app/controllers/users_controller.rb'
+        route = '  resources :users, :only => [:index, :show, :edit, :update ]'
+        inject_into_file 'config/routes.rb', route + "\n", :after => "root :to => \"visitors#index\"\n"
+        prepend_file 'app/views/users/_user.html.erb', "<td><%= link_to user.name, user %></td>\n"
+        inject_into_file 'app/views/users/show.html.erb', "\n<p>Name: <%= @user.name if @user.name %></p>", :before => "\n<p>Email"
+      end
+
+      def add_devise_name_field
+        return unless File.exists?('config/initializers/devise.rb')
         if Object.const_defined?('User')
           if User.column_names.include? 'name'
-            permitted = File.read(find_in_source_paths('permitted_parameters.rb'))
+            permitted = File.read(find_in_source_paths('devise/permitted_parameters.rb'))
             inject_into_file 'app/controllers/application_controller.rb', permitted + "\n", :after => "protect_from_forgery with: :exception\n"
-            prepend_file 'app/views/users/_user.html.erb', "<td><%= user.name %></td>\n"
+            prepend_file 'app/views/users/_user.html.erb', "<td><%= link_to user.name, user %></td>\n"
             inject_into_file 'app/views/users/show.html.erb', "\n<p>Name: <%= @user.name if @user.name %></p>", :before => "\n<p>Email"
           end
         end
       end
 
-      def add_tests
+      def add_devise_tests
+        return unless File.exists?('config/initializers/devise.rb')
         return unless File.exists?('spec/spec_helper.rb')
         copy_file 'spec/factories/users.rb', 'spec/factories/users.rb'
         copy_file 'spec/features/users/sign_in_spec.rb', 'spec/features/users/sign_in_spec.rb'
