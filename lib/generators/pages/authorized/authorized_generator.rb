76f8f77f1ddada5b81499382ce85f7e045e2e7c9
Daniel Kehoe <daniel@danielkehoe.com>
2014-06-19 10:39:39 -0700

    don't generate 'users' pages when generating 'authorized' pages

--- lib/generators/pages/authorized/authorized_generator.rb
+++ lib/generators/pages/authorized/authorized_generator.rb
@@ -10,7 +10,6 @@ module Pages
       def create_page
         ### assumes we are using Devise for authentication
         ### assumes we are using Pundit for authorization
-        generate 'pages:users -f'
         copy_file 'users/_user.html.erb', 'app/views/users/_user.html.erb'
         copy_file 'users_controller.rb', 'app/controllers/users_controller.rb'
         copy_file 'user_policy.rb', 'app/policies/user_policy.rb'
--- lib/rails_apps_pages/version.rb
+++ lib/rails_apps_pages/version.rb
@@ -1,3 +1,3 @@
 module RailsAppsPages
-  VERSION = "0.5.4"
+  VERSION = "0.5.5"
 end
