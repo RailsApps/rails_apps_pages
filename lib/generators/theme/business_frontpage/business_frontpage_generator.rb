36ef19bf27dcaf4c85757411d13a386e2e0b92b5
Daniel Kehoe <daniel@danielkehoe.com>
2015-05-10 22:50:57 +0800

    add 'Business Frontpage' theme from startbootstrap.com

---
+++ lib/generators/theme/business_frontpage/business_frontpage_generator.rb
@@ -0,0 +1,17 @@
+require 'rails/generators'
+
+module Theme
+  module Generators
+    class BusinessFrontpageGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "A Bootstrap HTML starter template (from startbootstrap.com)"
+
+      def create_page
+        copy_file 'index.html.erb', 'app/views/visitors/index.html.erb'
+        copy_file 'business-frontpage.css', 'app/assets/stylesheets/business-frontpage.css'
+      end
+
+    end
+  end
+end
