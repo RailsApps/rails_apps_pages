879bde12faefe74940aaa7312c610aee96018fcc
Daniel Kehoe <daniel@danielkehoe.com>
2015-05-10 19:29:59 +0800

    add 'Bare' theme from startbootstrap.com

---
+++ lib/generators/theme/bare/bare_generator.rb
@@ -0,0 +1,16 @@
+require 'rails/generators'
+
+module Theme
+  module Generators
+    class BareGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "A Bootstrap HTML starter template (from startbootstrap.com)"
+
+      def create_page
+        copy_file 'index.html.erb', 'app/views/visitors/index.html.erb'
+      end
+
+    end
+  end
+end
