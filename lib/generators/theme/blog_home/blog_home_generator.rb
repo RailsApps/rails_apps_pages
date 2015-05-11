682b2f1f4d137df165b1e6f5337688024c3c8f0b
Daniel Kehoe <daniel@danielkehoe.com>
2015-05-11 20:40:38 +0800

    add 'Blog Home' theme from startbootstrap.com

---
+++ lib/generators/theme/blog_home/blog_home_generator.rb
@@ -0,0 +1,17 @@
+require 'rails/generators'
+
+module Theme
+  module Generators
+    class BlogHomeGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "A Bootstrap HTML starter template (from startbootstrap.com)"
+
+      def create_page
+        copy_file 'index.html.erb', 'app/views/visitors/index.html.erb'
+        copy_file 'blog-home.css', 'app/assets/stylesheets/blog-home.css'
+      end
+
+    end
+  end
+end
