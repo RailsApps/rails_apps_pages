7deb2f6a5415656d539b58e05b1e374c45706d2f
Daniel Kehoe <daniel@danielkehoe.com>
2015-10-18 10:07:06 +0700

    add 'Business Casual' theme from startbootstrap.com

---
+++ lib/generators/theme/business_casual/business_casual_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+
+module Theme
+  module Generators
+    class BusinessCasualGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "A Bootstrap HTML starter template (from startbootstrap.com)"
+
+      def create_page
+        copy_file 'application.html.erb', 'app/views/layouts/application.html.erb'
+        copy_file 'index.html', 'app/views/visitors/index.html.erb'
+        copy_file 'business-casual.css', 'app/assets/stylesheets/business-casual.css'
+      end
+
+    end
+  end
+end
