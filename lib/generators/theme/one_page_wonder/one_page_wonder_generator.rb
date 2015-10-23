4797c98696ba75d5e34a88c3094f7bcef82c10c7
Daniel Kehoe <daniel@danielkehoe.com>
2015-10-23 14:50:04 +0700

    fix application layout for one_page_wonder theme

--- lib/generators/theme/one_page_wonder/one_page_wonder_generator.rb
+++ lib/generators/theme/one_page_wonder/one_page_wonder_generator.rb
@@ -11,12 +11,6 @@ module Theme
         copy_file 'application.html.erb', 'app/views/layouts/application.html.erb'
         copy_file 'index.html', 'app/views/visitors/index.html.erb'
         copy_file 'one-page-wonder.css', 'app/assets/stylesheets/one-page-wonder.css'
-        links = <<-LINKS
-<li><%= link_to 'Section 1', '#about' %></li>
-<li><%= link_to 'Section 2', '#services' %></li>
-<li><%= link_to 'Section 3', '#contact' %></li>
-LINKS
-        inject_into_file 'app/views/layouts/_navigation_links.html.erb', links, :after => "page_path('about') %></li>\n"
       end
 
     end
