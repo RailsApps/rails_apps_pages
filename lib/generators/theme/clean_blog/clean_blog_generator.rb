48194c8d4684a26ddbf080a836a0307cf506f776
Daniel Kehoe <daniel@danielkehoe.com>
2015-10-25 11:04:46 +0700

    improve clean_blog theme

--- lib/generators/theme/clean_blog/clean_blog_generator.rb
+++ lib/generators/theme/clean_blog/clean_blog_generator.rb
@@ -9,7 +9,13 @@ module Theme
 
       def create_page
         copy_file 'application.html.erb', 'app/views/layouts/application.html.erb'
+        copy_file 'footer.html', 'app/views/layouts/_footer.html.erb'
+        copy_file '_navigation.html.erb', 'app/views/layouts/_navigation.html.erb'
+        copy_file '_navigation_links.html.erb', 'app/views/layouts/_navigation_links.html.erb'
         copy_file 'index.html', 'app/views/visitors/index.html.erb'
+        copy_file 'about.html', 'app/views/pages/about.html.erb'
+        copy_file 'contact.html', 'app/views/pages/contact.html.erb'
+        copy_file 'post.html', 'app/views/pages/post.html.erb'
         copy_file 'clean-blog.css', 'app/assets/stylesheets/clean-blog.css'
       end
 
